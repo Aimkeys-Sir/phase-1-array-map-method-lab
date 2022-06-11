c7a6c1e6f83fd879a3da6450e1e0e9623e6f6ae3
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-06-11 13:02:23 +0300

    commit tree 0a4232093ec74ca76c26d99cc349a8595df1fcb0

--- index.js
+++ index.js
@@ -12,5 +12,38 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+return tutorials.map(sentence=>{
+  let sentenceArr=sentence.split(" ")
+  return sentenceArr.map(word=>word[0].toUpperCase()+word.substring(1,word.length)).join(" ")
+ })
 }
+
+// function caseForMe(sentence)
+// { 
+//   let sentenceArr=sentence.split(" ")
+//   return sentenceArr.map(word => word[0].toUpperCase()+word.substring(1,word.length)).join(" ")
+// }
+
+
+//console.log("00".toUpperCase())
+
+// //practice
+// const skiSchool=["aki","guadalupe","lei","aalam"]
+// const rollCall= skiSchool.map(student=>student+" the skier")
+
+// console.log(rollCall)
+// console.log("00w".toLowerCase)
+// const robots = [
+//   { name: "Johnny 5", modes: 5, isActivated: false },
+//   { name: "C3PO", modes: 3, isActivated: false },
+//   { name: "Sonny", modes: 2.5, isActivated: false },
+//   { name: "Baymax", modes: 1.5, isActivated: false },
+// ]
+
+// const onRobots=robots.map(robot=>{
+//   return Object.assign({},robot,
+//      {modes:robot.modes*2, isActivated:true})
+// })
+
+// console.log(onRobots)
+// console.log(robots)
